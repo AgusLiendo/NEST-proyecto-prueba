d4a7c65a42dd41719da07521a2f67020d7eade96
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2025-05-12 15:20:36 -0300

    fixeado la seleccion de la base de datos en el app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,7 +23,7 @@ import { UsersModule } from './users/users.module';
       port: +configService.get('DB_PORT'),
       username: configService.get('DB_USERNAME'),
       password: configService.get('DB_PASSWORD'),
-      database: configService.get('DB_NAME'),
+      database: configService.get('DB_DATABASE'),
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
