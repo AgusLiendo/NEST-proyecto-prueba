04b77115fd8dce012a13eaeeebe0e09976b816fd
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2025-05-12 23:14:44 -0300

    Actualizado el nombre del filtro de excepciones de TypeORM para usuarios

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { TypeOrmExceptionFilter } from './common/filters/typeorm-exception.filter';
+import { TypeOrmUsersExceptionFilter } from './common/filters/typeorm-users-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalFilters(new TypeOrmExceptionFilter());
+  app.useGlobalFilters(new TypeOrmUsersExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
