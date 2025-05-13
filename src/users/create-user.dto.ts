7af032f47caafcd4f0dfb0101a8c5f3b64d24ef5
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2025-05-13 10:58:13 -0300

    Actualizado mensaje de error de expresión regular para el campo name

--- src/users/create-user.dto.ts
+++ src/users/create-user.dto.ts
@@ -2,7 +2,7 @@ import { IsNotEmpty, Matches } from "class-validator";
 
 export class CreateUserDto {
     @IsNotEmpty({ message: 'El nombre no puede estar vacío' })
-    @Matches(/^[A-Za-z\s]+$/, { message: 'El nombre sólo puede contener letras (A–Z, a–z)'})
+    @Matches(/^[A-Za-z\s]+$/, { message: 'El nombre sólo puede contener letras Mayusculas y minúsculas' })
     name: string;
 
     @IsNotEmpty({ message: 'El email no puede estar vacío' })
